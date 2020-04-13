310657c4a8f21106a9560efbb763d763bf377ea7
0h-n0 <kbu94982@gmail.com>
2020-04-14 00:41:04 +0900

    feat: update

--- frontier_graph/__init__.py
+++ frontier_graph/__init__.py
@@ -1,1 +1,30 @@
+import typing
+
+import networkx as nx
+
 from . import frontier
+
+
+class NetworkxInterface:
+    def __init__(self, digraph: nx.DiGraph):
+        self.digraph = digraph
+        self.n_nodes = len(digraph)
+
+    def sample(self, starts: typing.List[int], ends: typing.List[int],
+               n_sample) -> typing.List[int]:
+        assert isinstance(self.n_nodes, int), "TypeError: {type(self.n_nodes}"
+        assert isinstance(starts, list), "TypeError: {type(starts}"
+        assert isinstance(ends, list), "TypeError: {type(ends}"
+        assert isinstance(n_sample, int), "TypeError: {type(n_sample}"
+        print(self.n_nodes)
+        print(list(self.digraph.edges())[0])
+        print(starts)
+        print(ends)
+        print(n_sample)
+        out = frontier.calc_frontier_combination(
+            self.n_nodes,
+            list(self.digraph.edges()),
+            starts,
+            ends,
+            n_sample)
+        return out
--- tests/test_frontier.py
+++ tests/test_frontier.py
@@ -1,9 +1,12 @@
+from unittest import TestCase
+
+import networkx as nx
+
 import frontier_graph.frontier as f
+from frontier_graph import NetworkxInterface
 
 
 def test_instance():
-    frontier = f.FrontierInterface(1)
-    print(frontier.method())
     out = f.calc_frontier_combination(
         4,
         [(1, 2), (1, 3), (2, 4), (3, 4)],
@@ -11,3 +14,15 @@ def test_instance():
         [4,],
         2)
     assert([[2, 4], [1, 3]], out)
+
+
+class TestNetworkxInterface(TestCase):
+    def test_sample(self):
+        g = nx.DiGraph()
+        g.add_edge(1, 2)
+        g.add_edge(1, 3)
+        g.add_edge(2, 4)
+        g.add_edge(3, 4)
+        ns = NetworkxInterface(g)
+        graphs = ns.sample([1,], [4,], 3)
+        assert([[2, 4], [1, 3]], graphs)
